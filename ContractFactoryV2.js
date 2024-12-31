a86b823ab5dcca6110b036c420c6337a4dc675fe
stephenstb <stephenstb@live.com>
2024-12-31 09:01:30 -0600

    Web3js update

--- ContractFactoryV2.js
+++ ContractFactoryV2.js
@@ -1,3 +1,4 @@
+//import { AbiItem } from "web3-utils";
 import NFT_Register from "../web3-data/contracts/NFT_Register.json" assert { "type": "json" };
 // Dapp
 import UserRegister from "../web3-data/contracts/UserRegister.json" assert { "type": "json" };
@@ -6,7 +7,7 @@ import Feed from "../web3-data/contracts/Feed.json" assert { "type": "json" };
 import FeedViewer from "../web3-data/contracts/FeedViewer.json" assert { "type": "json" };
 import CryptoRicks from "../web3-data/contracts/CryptoRicks.json" assert { "type": "json" };
 import VRFHelper from "../web3-data/contracts/VRFHelper.json" assert { "type": "json" };
-import Shop from "../web3-data/contracts/Shop.json" assert { "type": "json" };
+//import Shop from "../web3-data/contracts/Shop.json" assert {"type" : "json"}
 import MockUSDC from "../web3-data/contracts/MockUSDC.json" assert { "type": "json" };
 import StevesTees from "../web3-data/contracts/StevesTees.json" assert { "type": "json" };
 import AgeUpdater from "../web3-data/contracts/AgeUpdater.json" assert { "type": "json" };
@@ -25,35 +26,35 @@ import Call from "../web3-data/contracts/Call.json" assert { "type": "json" };
 import Name from "../web3-data/contracts/Name.json" assert { "type": "json" };
 export const contractFactoryV2 = (web3) => {
     return {
-        "NFT_Register": new web3.eth.Contract(NFT_Register.abi, "", { data: NFT_Register.bytecode }),
-        "UserRegister": new web3.eth.Contract(UserRegister.abi, "", { data: UserRegister.bytecode }),
-        "Messages": new web3.eth.Contract(Messages.abi, "", { data: Messages.bytecode }),
-        "Feed": new web3.eth.Contract(Feed.abi, "", { data: Feed.bytecode }),
-        "FeedViewer": new web3.eth.Contract(FeedViewer.abi, "", { data: FeedViewer.bytecode }),
-        "CryptoRicks": new web3.eth.Contract(CryptoRicks.abi, "", { data: CryptoRicks.bytecode }),
-        "VRFHelper": new web3.eth.Contract(VRFHelper.abi, "", { data: VRFHelper.bytecode }),
-        "Shop": new web3.eth.Contract(Shop.abi, "", { data: Shop.bytecode }),
-        "MockUSDC": new web3.eth.Contract(MockUSDC.abi, "", { data: MockUSDC.bytecode }),
-        "StevesTees": new web3.eth.Contract(StevesTees.abi, "", { data: StevesTees.bytecode }),
-        "AgeUpdater": new web3.eth.Contract(AgeUpdater.abi, "", { data: AgeUpdater.bytecode }),
-        "UserAgeManager": new web3.eth.Contract(UserAgeManager.abi, "", { data: UserAgeManager.bytecode }),
-        "MockAsset": new web3.eth.Contract(MockAsset.abi, "", { data: MockAsset.bytecode }),
-        "TransparentUpgradableProxy": new web3.eth.Contract(TransparentUpgradableProxy.abi, "", { data: TransparentUpgradableProxy.bytecode }),
-        "CometInterface": new web3.eth.Contract(CometInterface.abi, "", { data: CometInterface.bytecode }),
-        "LiquidatableQuery": new web3.eth.Contract(LiquidatableQuery.abi, "", { data: LiquidatableQuery.bytecode }),
-        "OnChainLiquidator": new web3.eth.Contract(OnChainLiquidator.abi, "", { data: OnChainLiquidator.bytecode }),
+        "NFT_Register": new web3.eth.Contract(NFT_Register.abi),
+        "UserRegister": new web3.eth.Contract(UserRegister.abi),
+        "Messages": new web3.eth.Contract(Messages.abi),
+        "Feed": new web3.eth.Contract(Feed.abi),
+        "FeedViewer": new web3.eth.Contract(FeedViewer.abi),
+        "CryptoRicks": new web3.eth.Contract(CryptoRicks.abi),
+        "VRFHelper": new web3.eth.Contract(VRFHelper.abi),
+        //"Shop": ( new web3.eth.Contract(Shop.abi as AbiItem[], "", {data: Shop.bytecode}) as unknown) as any,
+        "MockUSDC": new web3.eth.Contract(MockUSDC.abi),
+        "StevesTees": new web3.eth.Contract(StevesTees.abi),
+        "AgeUpdater": new web3.eth.Contract(AgeUpdater.abi),
+        "UserAgeManager": new web3.eth.Contract(UserAgeManager.abi),
+        "MockAsset": new web3.eth.Contract(MockAsset.abi),
+        "TransparentUpgradableProxy": new web3.eth.Contract(TransparentUpgradableProxy.abi),
+        "CometInterface": new web3.eth.Contract(CometInterface.abi),
+        "LiquidatableQuery": new web3.eth.Contract(LiquidatableQuery.abi),
+        "OnChainLiquidator": new web3.eth.Contract(OnChainLiquidator.abi),
         /** Uniswap v4 */
         "PoolManager": new web3.eth.Contract(PoolManager.abi, "", { data: PoolManager.bytecode }),
-        "BasicHook": new web3.eth.Contract(BasicHook.abi, "", { data: BasicHook.bytecode }),
+        "BasicHook": new web3.eth.Contract(BasicHook.abi),
         /* ens messages*/
-        "MessagesENS": new web3.eth.Contract(MessagesENS.abi, "", { data: MessagesENS.bytecode }),
+        "MessagesENS": new web3.eth.Contract(MessagesENS.abi),
         // uuid connect
-        "UUIDConnect": new web3.eth.Contract(UUIDConnect.abi, "", { data: UUIDConnect.bytecode }),
+        "UUIDConnect": new web3.eth.Contract(UUIDConnect.abi),
         // PublicKeys
-        "PublicKeys": new web3.eth.Contract(PublicKeys.abi, "", { data: PublicKeys.bytecode }),
+        "PublicKeys": new web3.eth.Contract(PublicKeys.abi, { data: PublicKeys.bytecode }),
         // Call
-        "Call": new web3.eth.Contract(Call.abi, "", { data: Call.bytecode }),
+        "Call": new web3.eth.Contract(Call.abi),
         // Name
-        "Name": new web3.eth.Contract(Name.abi, "", { data: Name.bytecode }),
+        "Name": new web3.eth.Contract(Name.abi),
     };
 };
--- ContractFactoryV2.ts
+++ ContractFactoryV2.ts
@@ -1,6 +1,6 @@
 
 
-import { AbiItem } from "web3-utils";
+//import { AbiItem } from "web3-utils";
 
 import Web3 from "web3"
 
@@ -19,7 +19,7 @@ import CryptoRicks from "../web3-data/contracts/CryptoRicks.json" assert {"type"
 
 import VRFHelper from "../web3-data/contracts/VRFHelper.json" assert {"type" : "json"}
 
-import Shop from "../web3-data/contracts/Shop.json" assert {"type" : "json"}
+//import Shop from "../web3-data/contracts/Shop.json" assert {"type" : "json"}
 
 import MockUSDC from "../web3-data/contracts/MockUSDC.json" assert {"type" : "json"}
 
@@ -60,43 +60,43 @@ export interface ContractFactoryV2{
 export const contractFactoryV2 = (web3 : Web3) : ContractFactoryV2 => {
 
     return {
-        "NFT_Register": (new web3.eth.Contract(NFT_Register.abi as AbiItem[], "" , {data: NFT_Register.bytecode}) as unknown) as any,
-        "UserRegister": (new web3.eth.Contract(UserRegister.abi as AbiItem[], "" , {data: UserRegister.bytecode}) as unknown) as any,
-        "Messages": (new web3.eth.Contract(Messages.abi as AbiItem[], "" , {data: Messages.bytecode}) as unknown) as any,
-        "Feed": (new web3.eth.Contract(Feed.abi as AbiItem[], "" , {data: Feed.bytecode}) as unknown) as any,
-        "FeedViewer": (new web3.eth.Contract(FeedViewer.abi as AbiItem[], "" , {data: FeedViewer.bytecode}) as unknown) as any,
-        "CryptoRicks": (new web3.eth.Contract(CryptoRicks.abi as AbiItem[], "" , {data: CryptoRicks.bytecode}) as unknown) as any,
-        "VRFHelper": (new web3.eth.Contract(VRFHelper.abi as AbiItem[], "" , {data: VRFHelper.bytecode}) as unknown) as any,
-        "Shop": ( new web3.eth.Contract(Shop.abi as AbiItem[], "", {data: Shop.bytecode}) as unknown) as any,
-        "MockUSDC": ( new web3.eth.Contract(MockUSDC.abi as AbiItem[], "", {data: MockUSDC.bytecode}) as unknown) as any,
-        "StevesTees": ( new web3.eth.Contract(StevesTees.abi as AbiItem[], "", {data: StevesTees.bytecode}) as unknown) as any,
-        "AgeUpdater": ( new web3.eth.Contract(AgeUpdater.abi as AbiItem[], "", {data: AgeUpdater.bytecode}) as unknown) as any,
-        "UserAgeManager": ( new web3.eth.Contract(UserAgeManager.abi as AbiItem[], "", {data: UserAgeManager.bytecode}) as unknown) as any,
+        "NFT_Register": (new web3.eth.Contract(NFT_Register.abi) as unknown) as any,
+        "UserRegister": (new web3.eth.Contract(UserRegister.abi) as unknown) as any,
+        "Messages": (new web3.eth.Contract(Messages.abi) as unknown) as any,
+        "Feed": (new web3.eth.Contract(Feed.abi ) as unknown) as any,
+        "FeedViewer": (new web3.eth.Contract(FeedViewer.abi ) as unknown) as any,
+        "CryptoRicks": (new web3.eth.Contract(CryptoRicks.abi ) as unknown) as any,
+        "VRFHelper": (new web3.eth.Contract(VRFHelper.abi ) as unknown) as any,
+        //"Shop": ( new web3.eth.Contract(Shop.abi as AbiItem[], "", {data: Shop.bytecode}) as unknown) as any,
+        "MockUSDC": ( new web3.eth.Contract(MockUSDC.abi ) as unknown) as any,
+        "StevesTees": ( new web3.eth.Contract(StevesTees.abi ) as unknown) as any,
+        "AgeUpdater": ( new web3.eth.Contract(AgeUpdater.abi) as unknown) as any,
+        "UserAgeManager": ( new web3.eth.Contract(UserAgeManager.abi ) as unknown) as any,
         
-        "MockAsset": ( new web3.eth.Contract(MockAsset.abi as AbiItem[], "", {data: MockAsset.bytecode}) as unknown) as any,
+        "MockAsset": ( new web3.eth.Contract(MockAsset.abi ) as unknown) as any,
 
-        "TransparentUpgradableProxy": ( new web3.eth.Contract(TransparentUpgradableProxy.abi as AbiItem[], "", {data: TransparentUpgradableProxy.bytecode}) as unknown) as any,
+        "TransparentUpgradableProxy": ( new web3.eth.Contract(TransparentUpgradableProxy.abi ) as unknown) as any,
 
-        "CometInterface": ( new web3.eth.Contract(CometInterface.abi as AbiItem[], "", {data: CometInterface.bytecode}) as unknown) as any,
-        "LiquidatableQuery": ( new web3.eth.Contract(LiquidatableQuery.abi as AbiItem[], "", {data: LiquidatableQuery.bytecode}) as unknown) as any,
-        "OnChainLiquidator": ( new web3.eth.Contract(OnChainLiquidator.abi as AbiItem[], "", {data: OnChainLiquidator.bytecode}) as unknown) as any,
+        "CometInterface": ( new web3.eth.Contract(CometInterface.abi ) as unknown) as any,
+        "LiquidatableQuery": ( new web3.eth.Contract(LiquidatableQuery.abi) as unknown) as any,
+        "OnChainLiquidator": ( new web3.eth.Contract(OnChainLiquidator.abi ) as unknown) as any,
         /** Uniswap v4 */
-        "PoolManager": ( new web3.eth.Contract(PoolManager.abi as AbiItem[], "", {data: PoolManager.bytecode}) as unknown) as any,
-        "BasicHook": ( new web3.eth.Contract(BasicHook.abi as AbiItem[], "", {data: BasicHook.bytecode}) as unknown) as any,
+        "PoolManager": ( new web3.eth.Contract(PoolManager.abi, "", {data: PoolManager.bytecode} ) as unknown) as any,
+        "BasicHook": ( new web3.eth.Contract(BasicHook.abi ) as unknown) as any,
 
         /* ens messages*/
-        "MessagesENS": ( new web3.eth.Contract(MessagesENS.abi as AbiItem[], "", {data: MessagesENS.bytecode}) as unknown) as any,
+        "MessagesENS": ( new web3.eth.Contract(MessagesENS.abi ) as unknown) as any,
 
         // uuid connect
-        "UUIDConnect": ( new web3.eth.Contract(UUIDConnect.abi as AbiItem[], "", {data: UUIDConnect.bytecode}) as unknown) as any,
+        "UUIDConnect": ( new web3.eth.Contract(UUIDConnect.abi ) as unknown) as any,
 
         // PublicKeys
-        "PublicKeys": ( new web3.eth.Contract(PublicKeys.abi as AbiItem[], "", {data: PublicKeys.bytecode}) as unknown) as any,
+        "PublicKeys": ( new web3.eth.Contract(PublicKeys.abi, {data: PublicKeys.bytecode} ) as unknown) as any,
         // Call
-        "Call": ( new web3.eth.Contract(Call.abi as AbiItem[], "", {data: Call.bytecode}) as unknown) as any,
+        "Call": ( new web3.eth.Contract(Call.abi ) as unknown) as any,
 
         // Name
-        "Name": ( new web3.eth.Contract(Name.abi as AbiItem[], "", {data: Name.bytecode}) as unknown) as any,
+        "Name": ( new web3.eth.Contract(Name.abi ) as unknown) as any,
 
     } as ContractFactoryV2
 }
